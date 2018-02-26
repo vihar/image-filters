3bfb755ad7b8c85f82901062bd87d868acb14ede
vihar <vihar.kurama@gmail.com>
2018-02-27 01:37:57 +0530

    Loop

--- loop.py
+++ loop.py
@@ -4,20 +4,6 @@ import numpy as np
 import scipy
 from scipy import stats
 
-from xlutils.copy import copy    
-from xlrd import open_workbook
-
-book_ro = open_workbook("variables.xlsx")
-book = copy(book_ro)  # creates a writeable copy
-sheet1 = book.get_sheet(0)  # get a first sheet
-
-colx = 1
-for rowx in range(1):
-    # Write the data to rox, column
-    sheet1.write(rowx,colx, url)
-    sheet1.write(rowx,colx+1, count)
-
-book.save("D:\Komal\MyPrograms\python_spreadsheet.xls")
 
 images = ['airplane.png']
 
